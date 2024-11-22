afc1ecf1f5593e38961177170d6a91f595553808
bandola78 <165742856+bandola78@users.noreply.github.com>
2024-11-22 22:57:51 +0800

    Update Dancer.java

--- Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/Dancer.java
+++ Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/Dancer.java
@@ -1,1 +1,30 @@
+enum DanceStyle {
+    BALLET,
+    HIPHOP,
+    JAZZ,
+    CONTEMPORARY
+}
 
+public class Dancer extends Artist {
+    private DanceStyle danceStyle;
+
+    public Dancer(String name, int age, String specialty, DanceStyle danceStyle) {
+        super(name, age, specialty);
+        this.danceStyle = danceStyle;
+    }
+
+    public DanceStyle getDanceStyle() {
+        return danceStyle;
+    }
+
+    public void setDanceStyle(DanceStyle danceStyle) { 
+        this.danceStyle = danceStyle;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Dancer Info:");
+        super.displayInfo();
+        System.out.println("Dance Style: " + danceStyle); 
+    }
+}
