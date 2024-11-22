47203f8dedde12ac23694bceda2b972aa6b249af
bandola78 <165742856+bandola78@users.noreply.github.com>
2024-11-22 22:58:57 +0800

    Update Writer.java

--- Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/Writer.java
+++ Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/Writer.java
@@ -1,1 +1,30 @@
+enum WritingStyle{
+    FICTION,
+    NONFICTION,
+    POETRY,
+    DRAMA,
+}
 
+public class Writer extends Artist {
+    private WritingStyle writing_style;
+
+    public Writer(String name, int age, String specialty, WritingStyle writingstyle) {
+        super(name, age, specialty);
+        this.writing_style = writingstyle;
+    }
+
+    public WritingStyle getWritingStyle() {
+        return writing_style;
+    }
+
+    public void setWritingStyle(WritingStyle writingstyle) {
+        this.writing_style = writingstyle;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Writer Info: ");
+        super.displayInfo();
+        System.out.println("Writing Style: " + writing_style);
+    }
+}
